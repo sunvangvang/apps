af09b773538ce3bcc032b4a9b4af03c41dc5e3f3
arvin <sunyy2012@sina.com>
2018-12-21 22:45:26 +0800

    Init project

---
+++ apps-core/src/main/java/com/aibyd/apps/core/Application.java
@@ -0,0 +1,15 @@
+package com.aibyd.apps.core;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = { "com.aibyd.apps" })
+@MapperScan("com.aibyd.apps.db.dao")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
