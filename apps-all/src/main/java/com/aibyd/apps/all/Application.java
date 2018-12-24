64196e9b3763276cb4a5f8e8ff565dea1b9d29bf
arvin <sunyy2012@sina.com>
2018-12-24 11:21:58 +0800

    Init Project

--- apps-all/src/main/java/com/aibyd/apps/all/Application.java
+++ apps-all/src/main/java/com/aibyd/apps/all/Application.java
@@ -3,9 +3,13 @@ package com.aibyd.apps.all;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(scanBasePackages = { "com.aibyd.apps" })
 @MapperScan("com.aibyd.apps.db.dao")
+@EnableTransactionManagement
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
